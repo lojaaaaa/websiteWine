769503fbfb1cb10f021ce547c0c8aa2bf55831b7
lojaaaaa <116946904+lojaaaaa@users.noreply.github.com>
2023-04-23 23:42:06 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const window_1 = document.querySelector('#window_1')
 const openWindow_1 = document.querySelector('#open_1')
 const closeWindow_1 = document.querySelector('#close_1')
-const body = document.querySelector('.wrapper')
+const body = document.querySelector('#body')
+const html = document.querySelector('html')
 const window_2 = document.querySelector('#window_2')
 const openWindow_2 = document.querySelector('#open_2')
 const closeWindow_2 = document.querySelector('#close_2')
@@ -12,12 +13,14 @@ openWindow_1.addEventListener('click', e =>{
     console.log(1)
     window_1.classList.add('active')
     body.classList.add('scroll__hidden')
+    html.classList.add('scroll__padding')
 })
 closeWindow_1.addEventListener('click', e =>{
     console.log(2)
     e.preventDefault()
     window_1.classList.remove('active')
     body.classList.remove('scroll__hidden')
+    html.classList.remove('scroll__padding')
 })
 
 
@@ -27,12 +30,14 @@ openWindow_2.addEventListener('click', e =>{
     console.log(1)
     window_2.classList.add('active')
     body.classList.add('scroll__hidden')
+    html.classList.add('scroll__padding')
 })
 closeWindow_2.addEventListener('click', e =>{
     console.log(2)
     e.preventDefault()
     window_2.classList.remove('active')
     body.classList.remove('scroll__hidden')
+    html.classList.remove('scroll__padding')
 })
 
 
@@ -50,3 +55,4 @@ for (let anchor of anchors){
     })
 
 }
+
